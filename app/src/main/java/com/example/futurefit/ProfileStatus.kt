23552ddd303f895a517131bb8f4dbeebd0da8d41
Aryan181206 <aryan180906@gmail.com>
2025-05-18 21:00:17 +0530

    1 week work commit 2

--- app/src/main/java/com/example/futurefit/ProfileStatus.kt
+++ app/src/main/java/com/example/futurefit/ProfileStatus.kt
@@ -1,10 +1,16 @@
 package com.example.futurefit
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.futurefit.Prediction.PredicitionSplashScreen
+import java.nio.file.Files.find
 
 class ProfileStatus : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +23,28 @@ class ProfileStatus : AppCompatActivity() {
             insets
         }
         //for tick and wrong directly search to firebase firestore
+
+
+        //initialisation
+        val checkname = findViewById<ImageView>(R.id.namecheck)
+        val checkage = findViewById<ImageView>(R.id.agecheck)
+        val checklocation = findViewById<ImageView>(R.id.locationcheck)
+        val checkstream = findViewById<ImageView>(R.id.streamcheck)
+        val checkqualification = findViewById<ImageView>(R.id.qualificationcheck)
+        val checkcgpa = findViewById<ImageView>(R.id.cgpacheck)
+        val checkaptitude = findViewById<ImageView>(R.id.aptitudecheck)
+        val checkpersonality = findViewById<ImageView>(R.id.personalitycheck)
+        val checktechnical = findViewById<ImageView>(R.id.technicalcheck)
+        val checksoft = findViewById<ImageView>(R.id.softcheck)
+        val checkinterest = findViewById<ImageView>(R.id.interestcheck)
+        val checkexperience = findViewById<ImageView>(R.id.experiencecheck)
+        val checkcertification = findViewById<ImageView>(R.id.certificationcheck)
+        val predicit = findViewById<CardView>(R.id.predicit)
+
+        predicit.setOnClickListener {
+            checkfromfirebase()
+            startActivity(Intent(this, PredicitionSplashScreen ::class.java))
+        }
 
     }
 }
